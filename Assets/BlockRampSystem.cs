c39db18d613b5096c2aa8a1342dcecb4f3237e06
Taylor Daviss <89953361+twdaviss@users.noreply.github.com>
2024-08-24 20:56:09 -0700

    Block Sliding Functional

--- Assets/BlockRampSystem.cs
+++ Assets/BlockRampSystem.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using UnityEngine.XR;
+public enum SurfaceFriction
+{
+    WoodOnWood,
+    WoodOnConcrete,
+    WoodOnMetal,
+    MetalOnConcrete,
+};
 public class BlockRampSystem : MonoBehaviour
 {
     [SerializeField] private PolygonCollider2D rampCollider;
     [SerializeField] private BoxCollider2D blockCollider;
+    [SerializeField] private float mass;
+    [SerializeField] private Vector2 gravity;
+    [SerializeField] private SurfaceFriction surfaceFriction = SurfaceFriction.WoodOnWood;
 
     private Vector2 topRampPoint;
     private Vector2 botRampPoint;
@@ -15,25 +26,67 @@ public class BlockRampSystem : MonoBehaviour
     private float xDiff;
     private float yDiff;
     private float slope;
+    private float yInt;
     private float rampLength;
+    private float blockWidth;
+    private float velocity = 0;
+
+    private float frictionCoefficient;
+
+    private Vector3 originalPosition;
 
     void Start()
     {
-        topRampPoint = rampCollider.points[1];
-        botRampPoint = rampCollider.points[2];
-        cornerRampPoint = rampCollider.points[0];
-        xDiff = Mathf.Abs(botRampPoint.x - topRampPoint.x);
-        yDiff = Mathf.Abs(botRampPoint.y - topRampPoint.y);
-        slope = yDiff / xDiff;
-        rampLength = Mathf.Sqrt(Mathf.Pow(xDiff,2) + Mathf.Pow(yDiff,2));
-
-        angle = -Mathf.Acos(xDiff / rampLength);
+        originalPosition = blockCollider.transform.position;
     }
-
     void Update()
     {
-        float yPos = cornerRampPoint.y + (slope * blockCollider.transform.position.x + topRampPoint.y);
-        blockCollider.transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * angle);
-        blockCollider.transform.position = new Vector3(blockCollider.transform.position.x, yPos, blockCollider.transform.position.z);
+        if (Input.GetMouseButtonDown(0)) 
+        { 
+            blockCollider.transform.position = originalPosition; 
+            velocity = 0;
+        }
+        topRampPoint = rampCollider.points[1] + (Vector2)rampCollider.transform.position;
+        botRampPoint = rampCollider.points[2] + (Vector2)rampCollider.transform.position;
+        blockWidth = blockCollider.gameObject.transform.localScale.x;
+        xDiff = botRampPoint.x - topRampPoint.x;
+        yDiff = botRampPoint.y - topRampPoint.y;
+        angle = Mathf.Atan2(yDiff, xDiff);
+
+        slope = yDiff / xDiff;
+        yInt = topRampPoint.y - (slope * topRampPoint.x);
+
+        float yPos = (slope * blockCollider.transform.position.x) + yInt;
+
+        switch (surfaceFriction)
+        {
+            case SurfaceFriction.WoodOnWood:
+                frictionCoefficient = 0.5f;
+                break;
+            case SurfaceFriction.WoodOnConcrete:
+                frictionCoefficient = 0.45f;
+                break;
+            case SurfaceFriction.WoodOnMetal:
+                frictionCoefficient = 0.3f;
+                break;
+            case SurfaceFriction.MetalOnConcrete:
+                frictionCoefficient = 0.6f;
+                break;
+        }
+
+        Vector2 fg = gravity * mass;
+        Vector2 fgy = fg * Mathf.Cos(-angle);
+        Vector2 fgx = fg * Mathf.Sin(-angle);
+        Vector2 fn = -fgy;
+        Vector2 ff = frictionCoefficient * fn;
+        if(Mathf.Abs(ff.magnitude) > fgx.magnitude) { ff = -fgx; }
+        float totalForce = fgx.magnitude - ff.magnitude;
+        float acceleration = totalForce/mass;
+
+        velocity += acceleration * Time.deltaTime * 0.01f;
+        float xVelocity = velocity * Mathf.Sin(-angle);
+        blockCollider.transform.position = new Vector3(blockCollider.transform.position.x + xVelocity, yPos + blockWidth / 2, 0);
+        blockCollider.transform.SetPositionAndRotation(new Vector3(blockCollider.transform.position.x + xVelocity, yPos + blockWidth / 2, 0), Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * angle));
+
     }
 }
